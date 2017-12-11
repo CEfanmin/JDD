bf9d6b9d527fdbdd320f6367ef8b2b06c6ab8ab7
fanmin <840831204@qq.com>
2017-12-11 17:16:36 +0800

    final model

--- code/jddMulti.py
+++ code/jddMulti.py
@@ -15,15 +15,15 @@ def loadData(source_file, sum_file, target_file, pre_file):
     target_data = pd.read_csv(target_file).fillna(0)
     pred_data = pd.read_csv(pre_file).replace([np.inf, -np.inf], 9999).fillna(0)
 
-    source_data_array = np.log1p(np.array(source_data)).reshape(90993,4,10,1)
-    sum_data_array = np.log1p(np.array(sum_data)).reshape(90993,1,1,9)
+    source_data_array = np.log1p(np.array(source_data)).reshape(90993,4,9,1)
+    sum_data_array = np.log1p(np.array(sum_data)).reshape(90993,1,1,4)
     target__data_array = np.array(target_data).reshape(90993, 1, 1, 1)
-    pred_data_array = np.log1p(np.array(pred_data)).reshape(90993, 4, 10, 1)
+    pred_data_array = np.log1p(np.array(pred_data)).reshape(90993, 4, 9, 1)
     return source_data_array, sum_data_array, target__data_array, pred_data_array
 
 
 source_data, sum_data, target_data, pred_data = loadData('../data/sourceData_8-10.csv',
-                                                '../data/userInfoSum_8-10.csv',
+                                                '../data/userAbility_8-10.csv',
 												'../data/targetData_11.csv',
 												'../data/preData_9-11.csv')
 
@@ -33,34 +33,32 @@ print("target shape is: ", target_data.shape)
 print("pred shape is:", pred_data.shape)
 
 # ConvNet
-month_input = Input(shape=(4,10, 1), dtype='float32',name='month_input')
-month_input1 = layers.Conv2D(32,(3,3), activation='relu')(month_input)
-month_input2 = layers.MaxPool2D(2,2)(month_input1)
+month_input = Input(shape=(4,9, 1), dtype='float32',name='month_input')
+month_input1 = layers.Conv2D(64,(3,3), activation='relu')(month_input)
+month_input2 = layers.MaxPool2D(2,1)(month_input1)
 month_input3 = layers.Dropout(0.2)(month_input2)
+
 month_input4 = layers.Conv2D(64,(1,3),activation='relu',\
             kernel_regularizer=regularizers.l2(l=0.001))(month_input3)
+month_input5 = layers.MaxPool2D(1,4)(month_input4)
 
-month_input5 = layers.MaxPool2D(1,2)(month_input4)
-
-# DenseNet
-sum_input = Input(shape=(1,1,9), dtype='float32', name='sum_input')
-sum_input1 = layers.Dense(128,activation='relu',kernel_regularizer=regularizers.l2(l=0.001))(sum_input)
+# Residual Net
+sum_input = Input(shape=(1,1,4), dtype='float32', name='sum_input')
+sum_input1 = layers.Dense(16,activation='relu')(sum_input)
 sum_input2 = layers.Dropout(0.2)(sum_input1)
 
-sum_input3 = layers.Dense(128, activation='relu',kernel_regularizer=regularizers.l2(l=0.001))(sum_input2)
-sum_input4 = layers.Dropout(0.5)(sum_input3)
+sum_input3 = layers.Dense(16, activation='relu')(sum_input2)
+sum_input4 = layers.Dropout(0.2)(sum_input3)
 
-sum_input5 = layers.Dense(128, activation='relu',kernel_regularizer=regularizers.l2(l=0.001))(sum_input4)
-sum_input6 = layers.Dropout(0.5)(sum_input5)
-y = layers.add([sum_input1, sum_input5]) 
+sum_input5 = layers.Dense(16, activation='relu')(sum_input4)
+res = layers.add([sum_input5, sum_input2]) 
 
-sum_input7 = layers.Dense(64,activation='relu',kernel_regularizer=regularizers.l2(l=0.001))(y)
+sum_input6 = layers.Dense(16,activation='relu',kernel_regularizer=regularizers.l2(l=0.001))(res)
 
 
 # Concat
-concatenated = layers.concatenate([month_input5, sum_input7], axis=-1)
+concatenated = layers.concatenate([month_input5, sum_input6], axis=-1)
 answer = layers.Dense(1, activation='relu')(concatenated)
-
 model = Model([month_input,sum_input], answer)
 model.compile(optimizer='adam',loss='mse')
 print("model summary is:",model.summary())
@@ -69,14 +67,14 @@ print("model summary is:",model.summary())
 model.fit({'month_input':source_data,'sum_input':sum_data},
             target_data,
             validation_split=0.1,
-            epochs=1000,
-            batch_size=128,
+            epochs=350,
+            batch_size=256,
             verbose=1
             )
 
 # prediction
-pre_sum_data = pd.read_csv('../data/userInfoSum_9-11.csv').replace([np.inf, -np.inf], 9999).fillna(0)
-pre_sum_data_array = np.log1p(np.array(pre_sum_data)).reshape(90993,1,1,9)
+pre_sum_data = pd.read_csv('../data/userAbility_9-11.csv').replace([np.inf, -np.inf], 9999).fillna(0)
+pre_sum_data_array = np.log1p(np.array(pre_sum_data)).reshape(90993,1,1,4)
 pre = model.predict({'month_input':pred_data,'sum_input':pre_sum_data_array})
 print("pre result done.")
 pre_pd = pd.DataFrame(np.array(pre).reshape(90993,1))
